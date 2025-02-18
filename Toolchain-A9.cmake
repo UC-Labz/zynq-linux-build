aaddb583c151c597750dbd5526e487fd5f596914
jmeza <noumenon@asu.edu>
2025-02-17 23:55:40 -0700

    Removed comments

--- Toolchain-A9.cmake
+++ Toolchain-A9.cmake
@@ -1,29 +1,7 @@
 # /**
 # --------------------------------------------------------------------------------
 # UNCLASSIFIED - UNLIMITED RIGHTS
-# --------------------------------------------------------------------------------
-#
-# Copyright 2023 Raytheon Company.
-#
-# This software was developed pursuant to Contract Number W31P4Q-19-C-0038 with
-# the U.S. government.  The U.S. government's rights in and to this copyrighted
-# software are specified in DFARS 252.227-7014, which was made part of the
-# above contract.
-#
-# Distribution Statement F. Further distribution only as directed by
-# PEO, Missiles & Space, Close Combat Weapon Systems Project Office,
-# ATTN: SFAE-MSL-CWE, Redstone Arsenal, AL 35898, or higher authority;
-# 14 Feb 17.
-#
-# WARNING - This document contains technical data whose export is restricted by
-#           the Arms Export Control Act (Title 22, U.S.C., Sec 2751, et seq.) or
-#           the Export Administration Act of 1979 (Title 50, U.S.C., App. 2401 et
-#           seq.), as amended. Violations of these export laws are subject to
-#           severe criminal penalties. Disseminate in accordance with provisions
-#           of DoD Directive 5230.25.
-# --------------------------------------------------------------------------------
-#  */
-
+# -------------------------------------------------------------------------------
 # find the appropriate compiler
 find_program( CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++ )
 if( NOT CMAKE_CXX_COMPILER )
